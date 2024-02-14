d3e763977b534e3b9b9ab2e2e62d315b21d95763
Jessirae Bufford <jessirae.bufford.439@gmail.com>
2024-02-14 14:32:21 -0700

    working on P1

--- P1/Program.cs
+++ P1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("FINALLY FIGURED IT OUT");
+﻿//program start
+//desearialize my lists, user and blogs
+//log in, if no login exists, create new user
+//send data to user list
+//user can:
+//      create a blog
+// if user creates blog, send to blog list
+//      associate user to blog in list
+//      user can have a list of blogs
+// at end of program, searialize list of user and blogs
+// when program starts again, go to top and repeat
